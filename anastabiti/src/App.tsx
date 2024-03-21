aaa11c72486ba13eab70a453891ac9e8e8f7cec2
anastabiti <anastabiti@gmail.com>
2024-03-21 15:56:01 +0000

    add dark mode

--- anastabiti/src/App.tsx
+++ anastabiti/src/App.tsx
@@ -20,18 +20,41 @@ function App() {
   }
   return (
     <>
-      {/* <h1 className="text-13xl font-bold underline">Backend developer</h1> */}
-      {/* <h1>Anas TABITI</h1> */}
+      <div style={{ position:"relative", top:"10px",left:"45%"}} className="lg:flex lg:gap-x-12">
+        <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+          About me
+        </a>
+        <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+          Projects
+        </a>
+        <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+          Download my CV
+        </a>
 
-      <button
-        style={{ position: "absolute" ,top:"10px", right:"10px"}}
-        onClick={handleClick}
-        type="button"
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      >
-        Dark/Light
-      </button>
+      </div>
 
+      {/* <div className="cursor-progress ..."> */}
+      <div  style={{ position:"relative", }} className="cursor-context-menu	 ...">
+        <h1  className="text-13xl font-bold underline">Backend developer</h1>
+        {/* https://tailwindcss.com/docs/box-decoration-break */}
+        <span
+          style={{ fontSize: "100px" }}
+          className="box-decoration-slice bg-gradient-to-r from-indigo-600 to-pink-500 text-white px-2 ..."
+        >
+          Anas
+          <br />
+          TABITI
+        </span>
+
+        <button
+          style={{ position: "absolute", top: "10px", right: "10px" }}
+          onClick={handleClick}
+          type="button"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Dark/Light
+        </button>
+      </div>
     </>
   );
 }
--- anastabiti/tailwind.config.js
+++ anastabiti/tailwind.config.js
@@ -5,8 +5,10 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+ 
   theme: {
     extend: {},
+    
   },
   plugins: [],
 }
