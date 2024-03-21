9a847aca0bed067f53a5903116823ac70af5badd
anastabiti <anastabiti@gmail.com>
2024-03-21 14:26:36 +0000

    setup my project with vite,react,tailwindcss

---
+++ anastabiti/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+// import './styles.css';
+//inspiration https://www.behance.net/gallery/157781767/Design-a-cool-portfolio-for-a-web-developer?tracking_source=search_projects|web+design+portfolio&l=8
+function App() {
+  return (
+    <>
+ <h1 className="text-13xl font-bold underline">Backend developer</h1>
+ {/* <h1>Anas TABITI</h1> */}
+    </>
+  )
+}
+
+export default App
