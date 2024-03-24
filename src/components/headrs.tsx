4e331c1b00d48ec6dc0abec686af8939440a6568
anastabiti <anastabiti@gmail.com>
2024-03-24 15:45:57 +0000

    add google analytics

--- src/components/headrs.tsx
+++ src/components/headrs.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactGA from "react-ga";
+// import ReactGA from 'react-ga';
 
 // <!-- https://flowbite.com/blocks/marketing/header/ -->
 const Header: React.FC = () => {
@@ -51,21 +51,19 @@ const Header: React.FC = () => {
                 </a>
               </li>
 
-              <li>
-                <a
-                  href="/anastabiti.pdf"
-                  download
-                  onClick={() =>
-                    ReactGA.event({
-                      category: "User",
-                      action: "Downloaded CV",
-                    })
-                  }
-                  className="block py-2 pr-4 pl-3 text-blue-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Download my CV
-                </a>
-              </li>
+<li>
+  <a
+    href="/anastabiti.pdf"
+    download
+    // onClick={() => ReactGA.event({
+    //   category: 'User',
+    //   action: 'Downloaded CV'
+    // })}
+    className="block py-2 pr-4 pl-3 text-blue-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+  >
+    Download my CV
+  </a>
+</li>
               {/* <li>
                 <a
                   href="/anastabiti.pdf"
